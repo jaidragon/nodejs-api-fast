ccd97925968c36ba6e60942e1d505eee02f7f625
Jai Kim <jaidragon@gmail.com>
2025-09-13 11:34:43 -0700

    3rd commmit Get/:id

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,6 +4,10 @@ const taskController = {};
 
 /** 
 {
+   http://localhost:5000/api/tasks/  POST
+   http://localhost:5000/api/tasks/68c58518d523cdc56f2aeb3d Put, Delete, 
+   http://localhost:5000/api/tasks/68c58518d523cdc56f2aeb3d Get
+
   "task": "third-2 data",
   "isComplete": false
 }
@@ -31,6 +35,19 @@ taskController.getTasks = async (req, res) => {
     }
 };
 
+taskController.getTaskbyId = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const task = await Task.findById(id);
+        if (!task) {
+            throw new Error("Task not found");
+        }
+        return res.status(200).json({ status: "ok", data: task });
+    } catch (error) {   
+        return res.status(400).json({ status: "fail", message: error.message });
+    }
+}
+
 taskController.updateTask = async (req, res) => {
     try {
         const id = req.params.id;
@@ -43,7 +60,7 @@ taskController.updateTask = async (req, res) => {
         taskDB.task = task;
         taskDB.isComplete = isComplete;
         await taskDB.save();
-            return res.status(200).json({ status: "ok", data: taskDB });
+        return res.status(200).json({ status: "ok", data: taskDB });
 
     } catch (error) {   
         return res.status(400).json({ status: "fail", message: error.message });
@@ -61,7 +78,5 @@ taskController.deleteTask = async (req, res) => {
         return res.status(400).json({ status: "fail", message: error.message });
     }
 };
-
-
 
 module.exports = taskController;
--- routes/task.api.js
+++ routes/task.api.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/taskController');
+const { createTask, getTasks, updateTask, deleteTask, getTaskbyId } = require('../controllers/taskController');
 
 const router = express.Router();
 
 
 router.get("/", getTasks);
+router.get("/:id", getTaskbyId);
 router.post("/", createTask);
 router.put("/:id", updateTask, );
 router.delete("/:id", deleteTask);
