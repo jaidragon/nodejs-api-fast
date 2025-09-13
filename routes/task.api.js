d5ded1e2b7d1980ca96c31d9e4577440531220a8
Jai Kim <jaidragon@gmail.com>
2025-09-13 11:41:18 -0700

    4th commit .get.post

--- routes/task.api.js
+++ routes/task.api.js
@@ -4,10 +4,13 @@ const { createTask, getTasks, updateTask, deleteTask, getTaskbyId } = require('.
 const router = express.Router();
 
 
-router.get("/", getTasks);
-router.get("/:id", getTaskbyId);
-router.post("/", createTask);
-router.put("/:id", updateTask, );
-router.delete("/:id", deleteTask);
+// router.get("/", getTasks);
+// router.get("/:id", getTaskbyId);
+// router.post("/", createTask);
+// router.put("/:id", updateTask);
+// router.delete("/:id", deleteTask);
+
+router.get("/", getTasks).get("/:id", getTaskbyId).post("/", createTask).put("/:id", updateTask).delete("/:id", deleteTask);
+
 
 module.exports = router;
